cb5fba3c3b07053d4ad90836088b8a44de0a04b3
Doly Jimenez <doly.jimenez11@gmail.com>
2022-01-29 00:26:37 -0500

    Redirige la ruta principal a la carpeta dist

--- src/server.js
+++ src/server.js
@@ -4,16 +4,14 @@ const path = require('path');
 const app = express();
 
 //archivos estaticos del directorio dist
-var dirPath = __dirname +'/dist/mercado-libre-angular';
-app.use(express.static(__dirname+'/dist/mercado-libre-angular'));
+var dirPath = __dirname +'../dist/mercado-libre-angular';
+app.use(express.static(dirPath));
 console.error("Directorio:" +__dirname);
 console.error("Directorio mecado libre:" +dirPath);
 
-var dirPath = __dirname +'/dist/mercado-libre-angular';
-
 //Enviar los requests al index.html
 app.get('/',function(req,res){
-  var indexPath = path.join(__dirname+'/dist/mercado-libre-angular/index.html');
+  var indexPath = path.join(__dirname+'../dist/mercado-libre-angular/index.html');
   console.error("Index path:" + indexPath)
   res.sendFile(indexPath);
 });
